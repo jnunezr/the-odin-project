d3e693f796087aba2431f9832a473dd937cba70b
jnunezr <rrjohan91@gmail.com>
2025-04-25 14:51:34 -0600

    uncomment main.rb lines to gets player names from console

--- tic_tac_toe_game/main.rb
+++ tic_tac_toe_game/main.rb
@@ -2,10 +2,10 @@
 
 require_relative 'lib/game'
 
-# puts 'Please enter the name of player 1?'
-# name1 = gets.chomp
-# puts 'Please enter the name of player 2?'
-# name2 = gets.chomp
+puts 'Please enter the name of player 1?'
+name1 = gets.chomp
+puts 'Please enter the name of player 2?'
+name2 = gets.chomp
 
 game = Game.new(name1 = 'Player 1', name2 = 'Player 2')
 game.play
